96a59848097db61eb6aa22ea6809649a2aa8e2bc
AndrewSultan <andrew15sultan@gmail.com>
2023-09-10 10:56:40 +0300

    add long_description_content_type to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-README = read('README.md')
 setup(
     name='nisip',
     version='0.0.1',
@@ -13,7 +12,7 @@ setup(
     author='Andrei Zoltan',
     license='GPLv2',
     packages=find_packages(exclude=['tests', 'docs']),
-    long_description=README,
     description='A Python package for sandpile models.',
+    long_description_content_type="text/markdown",
     long_description=read('README.md'),
 )
