5250a54213e8025cbd0e4348d490212ad1786bd7
AndreiZoltan <andrew15sultan@gmail.com>
2023-09-13 19:47:27 +0300

    refs/tags/v0.0.2 python version

---
+++ nisip/visualization/imshow.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+from nisip.sandpiles.sandpile import Sandpile
+
+def imshow(sandpile: Sandpile) -> None:
+    """
+    Display the sandpile.
+    """
+    import matplotlib.pyplot as plt # TODO : move this import to the top of the file
+    if sandpile.tiling == 'square':
+        plt.imshow(sandpile.graph, cmap='hot', interpolation='nearest')
+        plt.show()
