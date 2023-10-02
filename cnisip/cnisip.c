5d717bf1aa401832c576b83cea1c84cfca6e708b
AndreiZoltan <andrew15sultan@gmail.com>
2023-10-02 20:41:25 +0300

    improve memory management

--- cnisip/cnisip.c
+++ cnisip/cnisip.c
@@ -34,34 +34,25 @@ static PyObject *relax_square(PyObject *self, PyObject *args)
         return NULL;
     }
     
-    long long **_data;
+    long long **data;
     int64_t size = PyArray_SIZE(arr);
     npy_intp *dims = PyArray_DIMS(arr);
     arr = PyArray_Cast(arr, NPY_INT64);
-    PyArray_AsCArray((PyObject **)&arr, &_data, dims, 2, PyArray_DescrFromType(NPY_INT64));
+    PyArray_AsCArray((PyObject **)&arr, &data, dims, 2, PyArray_DescrFromType(NPY_INT64));
     if (PyErr_Occurred())
     {
         return NULL;
     }
-    long long *data;
-    data = malloc(size*sizeof(long long));
-    for (int i = 0; i < (int)dims[0]; i++)
-    {
-        for(int j = 0; j < (int)dims[1]; j++)
-        {
-            data[i*dims[0] + j] = _data[i][j];
-        }
-    }
-    relax_square_trivial_boundary(data, (int)dims[0], (int)dims[1]);
     PyObject *result = PyArray_SimpleNew(2, dims, NPY_INT64);
     long long *result_data = PyArray_DATA((PyArrayObject *)result);
     for (int i = 0; i < (int)dims[0]; i++)
     {
         for(int j = 0; j < (int)dims[1]; j++)
         {
-            result_data[i*dims[1] + j] = data[i*dims[1] + j];
+            result_data[i*dims[0] + j] = data[i][j];
         }
     }
+    relax_square_trivial_boundary(result_data, (int)dims[0], (int)dims[1]);
     return result;
 };
 
@@ -84,34 +75,25 @@ static PyObject *relax_triangular(PyObject *self, PyObject *args)
         return NULL;
     }
     
-    long long **_data;
+    long long **data;
     int64_t size = PyArray_SIZE(arr);
     npy_intp *dims = PyArray_DIMS(arr);
     arr = PyArray_Cast(arr, NPY_INT64);
-    PyArray_AsCArray((PyObject **)&arr, &_data, dims, 2, PyArray_DescrFromType(NPY_INT64));
+    PyArray_AsCArray((PyObject **)&arr, &data, dims, 2, PyArray_DescrFromType(NPY_INT64));
     if (PyErr_Occurred())
     {
         return NULL;
     }
-    long long *data;
-    data = malloc(size*sizeof(long long));
-    for (int i = 0; i < (int)dims[0]; i++)
-    {
-        for(int j = 0; j < (int)dims[1]; j++)
-        {
-            data[i*dims[1] + j] = _data[i][j];
-        }
-    }
-    relax_triangular_trivial_boundary(data, (int)dims[0], (int)dims[1]);
     PyObject *result = PyArray_SimpleNew(2, dims, NPY_INT64);
     long long *result_data = PyArray_DATA((PyArrayObject *)result);
     for (int i = 0; i < (int)dims[0]; i++)
     {
         for(int j = 0; j < (int)dims[1]; j++)
         {
-            result_data[i*dims[1] + j] = data[i*dims[1] + j];
+            result_data[i*dims[1] + j] = data[i][j];
         }
     }
+    relax_triangular_trivial_boundary(result_data, (int)dims[0], (int)dims[1]);
     return result;
 };
 
