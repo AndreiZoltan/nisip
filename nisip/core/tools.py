ccb5a974caf3254481a7832814c4288711d0bb20
AndreiZoltan <andrew15sultan@gmail.com>
2023-10-16 00:12:32 +0300

    add coridors

--- nisip/core/tools.py
+++ nisip/core/tools.py
@@ -43,33 +43,71 @@ def charge_graph(x: int, y: int, shape: tuple) -> np.ndarray:
     graph = np.zeros(shape, dtype=np.int64)
     # graph[x, y+1] = 240
     grid = np.indices(shape)
-    graph[:x+1, y:-1] |= 1<<0
-    graph[1:x+1, y:] |= 1<<3
-    graph[:x, y+1:-1] |= np.where(grid[0] + grid[1] >= x+y, 1<<4, 0)[:x, y+1:-1]
-    graph[1:x, y+1:] |= np.where(grid[0] + grid[1] <= x+y, 1<<5, 0)[1:x, y+1:]
+    graph[: x + 1, y:-1] |= 1 << 0
+    graph[1 : x + 1, y:] |= 1 << 3
+    graph[:x, y + 1 : -1] |= np.where(grid[0] + grid[1] >= x + y, 1 << 4, 0)[
+        :x, y + 1 : -1
+    ]
+    graph[1:x, y + 1 :] |= np.where(grid[0] + grid[1] <= x + y, 1 << 5, 0)[1:x, y + 1 :]
 
-    graph[x:, 1:y+1] |= 1<<1
-    graph[x:-1, :y+1] |= 1<<2
-    graph[x+1:-1, :y] |= np.where(grid[0] + grid[1] >= x+y, 1<<4, 0)[x+1:-1, :y]
-    graph[x+1:, 1:y] |= np.where(grid[0] + grid[1] <= x+y, 1<<5, 0)[x+1:, 1:y]
+    graph[x:, 1 : y + 1] |= 1 << 1
+    graph[x:-1, : y + 1] |= 1 << 2
+    graph[x + 1 : -1, :y] |= np.where(grid[0] + grid[1] >= x + y, 1 << 4, 0)[
+        x + 1 : -1, :y
+    ]
+    graph[x + 1 :, 1:y] |= np.where(grid[0] + grid[1] <= x + y, 1 << 5, 0)[x + 1 :, 1:y]
 
-    graph[:x+1, 1:y+1] |= np.where(grid[1] <= grid[0] + y - x, 1<<1, 0)[:x+1, 1:y+1]
-    graph[1:x+1, 1:y+1] |= np.where(grid[1] <= grid[0] + y - x, 1<<5, 0)[1:x+1, 1:y+1]
-    graph[1:x, :y] |= np.where((grid[1] >= 2*grid[0] - 2*x + y) & (grid[1] < grid[0] + y - x), 1<<3, 0)[1:x, :y]
-    graph[:x, :y] |= np.where(grid[1] <= 2*grid[0] - 2*x + y, 1<<2, 0)[:x, :y]
+    graph[: x + 1, 1 : y + 1] |= np.where(grid[1] <= grid[0] + y - x, 1 << 1, 0)[
+        : x + 1, 1 : y + 1
+    ]
+    graph[1 : x + 1, 1 : y + 1] |= np.where(grid[1] <= grid[0] + y - x, 1 << 5, 0)[
+        1 : x + 1, 1 : y + 1
+    ]
+    graph[1:x, :y] |= np.where(
+        (grid[1] >= 2 * grid[0] - 2 * x + y) & (grid[1] < grid[0] + y - x), 1 << 3, 0
+    )[1:x, :y]
+    graph[:x, :y] |= np.where(grid[1] <= 2 * grid[0] - 2 * x + y, 1 << 2, 0)[:x, :y]
 
-    graph[1:x+1, :y+1] |= np.where(grid[1] >= grid[0] + y - x, 1<<3, 0)[1:x+1, :y+1]
-    graph[1:x+1, 1:y+1] |= np.where(grid[1] >= grid[0] + y - x, 1<<5, 0)[1:x+1, 1:y+1]
-    graph[:x, :y] |= np.where(grid[1] >= grid[0]/2 - x/2 + y, 1<<0, 0)[:x, :y]
-    graph[:x, 1:y] |= np.where((grid[1] <= grid[0]/2 - x/2 + y) & (grid[1] > grid[0] + y - x), 1<<1, 0)[:x, 1:y]
-    
-    graph[x:, y:-1] |= np.where(grid[1] >= grid[0] + y - x, 1<<0, 0)[x:, y:-1]
-    graph[x:-1, y:-1] |= np.where(grid[1] >= grid[0] + y - x, 1<<4, 0)[x:-1, y:-1]
-    graph[x+1:, y+1:] |= np.where(grid[1] >= 2*grid[0] - 2*x + y, 1<<3, 0)[x+1:, y+1:]
-    graph[x+1:-1, y+1:] |= np.where((grid[1] <= 2*grid[0] - 2*x + y) & (grid[1] > grid[0] + y - x), 1<<2, 0)[x+1:-1, y+1:]
-    
-    graph[x:-1, y:] |= np.where(grid[1] <= grid[0] + y - x, 1<<2, 0)[x:-1, y:]
-    graph[x:-1, y:-1] |= np.where(grid[1] <= grid[0] + y - x, 1<<4, 0)[x:-1, y:-1]
-    graph[x+1:, y+1:] |= np.where(grid[1] <= grid[0]/2 - x/2 + y, 1<<1, 0)[x+1:, y+1:]
-    graph[x+1:, y+1:-1] |= np.where((grid[1] >= grid[0]/2 - x/2 + y) & (grid[1] < grid[0] + y - x), 1<<0, 0)[x+1:, y+1:-1]
+    graph[1 : x + 1, : y + 1] |= np.where(grid[1] >= grid[0] + y - x, 1 << 3, 0)[
+        1 : x + 1, : y + 1
+    ]
+    graph[1 : x + 1, 1 : y + 1] |= np.where(grid[1] >= grid[0] + y - x, 1 << 5, 0)[
+        1 : x + 1, 1 : y + 1
+    ]
+    graph[:x, :y] |= np.where(grid[1] >= grid[0] / 2 - x / 2 + y, 1 << 0, 0)[:x, :y]
+    graph[:x, 1:y] |= np.where(
+        (grid[1] <= grid[0] / 2 - x / 2 + y) & (grid[1] > grid[0] + y - x), 1 << 1, 0
+    )[:x, 1:y]
+
+    graph[x:, y:-1] |= np.where(grid[1] >= grid[0] + y - x, 1 << 0, 0)[x:, y:-1]
+    graph[x:-1, y:-1] |= np.where(grid[1] >= grid[0] + y - x, 1 << 4, 0)[x:-1, y:-1]
+    graph[x + 1 :, y + 1 :] |= np.where(grid[1] >= 2 * grid[0] - 2 * x + y, 1 << 3, 0)[
+        x + 1 :, y + 1 :
+    ]
+    graph[x + 1 : -1, y + 1 :] |= np.where(
+        (grid[1] <= 2 * grid[0] - 2 * x + y) & (grid[1] > grid[0] + y - x), 1 << 2, 0
+    )[x + 1 : -1, y + 1 :]
+
+    graph[x:-1, y:] |= np.where(grid[1] <= grid[0] + y - x, 1 << 2, 0)[x:-1, y:]
+    graph[x:-1, y:-1] |= np.where(grid[1] <= grid[0] + y - x, 1 << 4, 0)[x:-1, y:-1]
+    graph[x + 1 :, y + 1 :] |= np.where(grid[1] <= grid[0] / 2 - x / 2 + y, 1 << 1, 0)[
+        x + 1 :, y + 1 :
+    ]
+    graph[x + 1 :, y + 1 : -1] |= np.where(
+        (grid[1] >= grid[0] / 2 - x / 2 + y) & (grid[1] < grid[0] + y - x), 1 << 0, 0
+    )[x + 1 :, y + 1 : -1]
+    return graph
+
+
+def charge_graph_with_corridors(
+    x: int, y: int, stride: int, shape: tuple
+) -> np.ndarray:
+    grid = np.indices(shape)
+    graph = charge_graph(x, y, shape)
+    for i in range(x % stride, shape[0], stride):
+        graph[i] |= 0b111111
+    for i in range(y % stride, shape[1], stride):
+        graph[:, i] |= 0b111111
+    for i in range(-shape[0] + ((shape[0] + y - x) % stride), np.sum(shape), stride):
+        graph |= np.where(grid[1] == grid[0] + y - x + i, 0b111111, 0)
     return graph
