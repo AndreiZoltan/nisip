daf855845e77bfff9726ca1479f8dd6f571003db
Andrei Zoltan <andrew15sultan@gmail.com>
2023-11-30 12:57:49 +0000

    improve relax performance

--- nisip/core/relax.py
+++ nisip/core/relax.py
@@ -1,7 +1,6 @@
 from copy import deepcopy
 
 import numpy as np
-import numba
 from numba import jit
 
 from nisip.sandpiles import Sandpile
@@ -14,35 +13,50 @@ def relax_directed(sandpile: Sandpile) -> np.ndarray:
         sandpile.nodes_degrees != 0, sandpile.nodes_degrees, 6
     ).astype(np.int32)
     sandpile.graph[sandpile.boundary != 0] = 0
+    shift_masks = np.zeros((6, *sandpile.graph.shape), dtype=np.bool_)
+    for i in range(6):
+        shift_masks[i] = (sandpile.directed_graph & (1 << i)) != 0
     while (sandpile.graph >= nodes_degrees).any():
         mask = sandpile.graph >= nodes_degrees
         pile = np.where(mask, sandpile.graph, 0)
         sandpile.graph[mask] = pile[mask] % nodes_degrees[mask]
-        pile = pile // nodes_degrees  # TODO try to use mask
-        sandpile.graph[:, 1:] += np.where(
-            sandpile.directed_graph[:, :-1] & (1 << 0) != 0, pile[:, :-1], 0
-        )
-        sandpile.graph[:, :-1] += np.where(
-            sandpile.directed_graph[:, 1:] & (1 << 1) != 0, pile[:, 1:], 0
-        )
-        sandpile.graph[1:, :] += np.where(
-            sandpile.directed_graph[:-1, :] & (1 << 2) != 0, pile[:-1, :], 0
-        )
-        sandpile.graph[:-1, :] += np.where(
-            sandpile.directed_graph[1:, :] & (1 << 3) != 0, pile[1:, :], 0
-        )
-        sandpile.graph[1:, 1:] += np.where(
-            sandpile.directed_graph[:-1, :-1] & (1 << 4) != 0, pile[:-1, :-1], 0
-        )
-        sandpile.graph[:-1, :-1] += np.where(
-            sandpile.directed_graph[1:, 1:] & (1 << 5) != 0, pile[1:, 1:], 0
-        )
+        pile = pile // nodes_degrees
+        shifts = mask & shift_masks
+        sandpile.graph[:, 1:][shifts[0, :, :-1]] += pile[shifts[0]]
+        sandpile.graph[:, :-1][shifts[1, :, 1:]] += pile[shifts[1]]
+        sandpile.graph[1:, :][shifts[2, :-1, :]] += pile[shifts[2]]
+        sandpile.graph[:-1, :][shifts[3, 1:, :]] += pile[shifts[3]]
+        sandpile.graph[1:, 1:][shifts[4, :-1, :-1]] += pile[shifts[4]]
+        sandpile.graph[:-1, :-1][shifts[5, 1:, 1:]] += pile[shifts[5]]
         sandpile.graph[sandpile.boundary != 0] = 0
     sandpile.graph[sandpile.nodes_degrees == 0] = 0
     return sandpile.graph
 
 
 def relax(sandpile: Sandpile) -> Sandpile:
+    """
+    Relax the sandpile until it is stable.
+
+    Parameters
+    ----------
+    sandpile : Sandpile
+        The sandpile to relax.
+
+    Returns
+    -------
+    Sandpile
+        The relaxed sandpile.
+    """
+    sandpile = deepcopy(sandpile)
+    if sandpile.tiling == "square":
+        if sandpile.is_trivial_boundary:
+            sandpile.set_graph(cns.relax_square(sandpile.graph))
+    elif sandpile.tiling == "triangular":
+        sandpile.set_graph(relax_directed(sandpile))
+    return sandpile
+
+
+def _relax(sandpile: Sandpile) -> Sandpile:
     """
     Relax the sandpile until it is stable.
 
